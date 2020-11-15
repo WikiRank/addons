b81a064d5c7fa6db093e04302d53f6a002df5a2f
WikiRank <73789827+WikiRank@users.noreply.github.com>
2020-11-15 20:04:32 +0100

    WikiRank Chrome add-on ver. 1.1

--- chrome/wikirank.js
+++ chrome/wikirank.js
@@ -1,11 +1,24 @@
+document.body.classList.add("wikirank-visited");
 chrome.browserAction.onClicked.addListener(function() {
 chrome.tabs.getSelected(null, function(tab) {
-
-      var tabUrl = tab.url;
+var langz = chrome.i18n.getUILanguage();
+var aditoUrl="";
+if (langz=="pl") {aditoUrl="pl.";}
+else if (langz=="ru") {aditoUrl="ru.";}
+var tabUrl = tab.url;
+if (tabUrl.match(/h?t?t?p?s?:?\/?\/?[a-z]{2}[.]m?[.]?wikipedia[.]org\/wiki\/.+/g)) {
   chrome.tabs.create({
-    'url':"https://wikirank.net/go.php?articlename="+tabUrl
-  });   
-    });
-  });
+    'url':"https://"+aditoUrl+"wikirank.net/go.php?articlename="+tabUrl
+  }); 
+} else {
+if (langz=="pl") {alert("Ta strona nie jest artykułem Wikipedii");}
+else if (langz=="ru") {alert("Эта страница не является статьей Википедии");}
+else {alert("This page is not a Wikipedia article");}
+
+}
+});
+});
+
+
   
   
